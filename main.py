214e0f43e92925f80f0435c6b20eb526e67fdc45
jabbour <jaboordandan14@gmail.com>
2025-11-14 12:54:52 +0200

    Convert project to FastAPI

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def hello_world(request):
-    return "Hello from GitHub repo!"
+from fastapi import FastAPI
+import uvicorn
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message": "Hello from FastAPI on Cloud Run via GitHub!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
